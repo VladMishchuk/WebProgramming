dc22d4f570a56e051007849df63f67fd507725e9
Vlad Mishchyk <124392538+VladMishchuk@users.noreply.github.com>
2023-04-06 12:01:41 +0000

    Js lab1

---
+++ web/js/Lab1/script.js
@@ -0,0 +1,6 @@
+function equation(){
+    let a = parseInt(document.getElementById("a").value);
+    let b = parseInt(document.getElementById("b").value);
+
+    document.getElementById("y").value = b / a;
+}
