bc57c0662b3c74953d7bdc13e2b1e417bd7e6ef2
Vlad Mishchyk <124392538+VladMishchuk@users.noreply.github.com>
2023-04-19 19:26:39 +0000

    Lab 7, April, 19

---
+++ web/js/Lab7(chocolate)/script.js
@@ -0,0 +1,39 @@
+let
+  inputs = document.querySelectorAll(".input"),
+  val;
+    
+function check() {
+  let 
+    height = document.getElementById("m").value,
+    width = document.getElementById("n").value,
+    pieces = document.getElementById("k").value,
+    chocolate = width * height,
+    output;
+ 
+  if (height % pieces === 0 | width  % pieces === 0) {
+    while(chocolate > 0) {
+      height -= pieces
+    }
+  }
+  console.log(height)
+  console.log(5 % 1)
+  /*output =  
+    height === '' | 
+    height === '0' | 
+    width  === '' |
+    pieces === ''
+      ? alert("Введіть усі дані")
+      : height % pieces === 0 |
+        width  % pieces === 0
+          ? "можна"
+          : "не можна"
+  */
+  document.getElementById("y").innerHTML = output
+}
+
+inputs.forEach(input => {
+    input.addEventListener("input", e => {
+        val = input.value.replace(/[^0-9]/g, '');
+        input.value = val;
+    });
+});
