70bab273b8725f01130e55424495f01c39c211d0
Vlad Mishchyk <124392538+VladMishchuk@users.noreply.github.com>
2023-04-19 17:22:00 +0000

    Create lab7

--- web/js/Lab1/script.js
+++ web/js/Lab1/script.js
@@ -4,14 +4,19 @@ let
 
 function equation(){
     let a = parseInt(document.getElementById("a").value),
-        b = parseInt(document.getElementById("b").value),
-        y = document.getElementById("y").innerHTML;
+        b = parseInt(document.getElementById("b").value);
 
-    a === 0 & b === 0
-        ? document.getElementById("y").innerHTML = "Будь яке число є розв'язком"
-        : a === 0 & b !== 0
-            ? document.getElementById("y").innerHTML = "Жодного розв'язку немає"
-            : document.getElementById("y").innerHTML = b / a
+        console.log(a)
+
+    isNaN(a) | isNaN(b)
+        ? alert("Введіть усі значенння")
+        : a === 0 & b === 0
+            ? document.getElementById("y").innerHTML = "Будь яке число є розв'язком"
+            : a === 0 & b !== 0
+                ? document.getElementById("y").innerHTML = "Жодного розв'язку немає"
+                : document.getElementById("y").innerHTML = b / a
+
+
 }
 inputs.forEach(input => {
     input.addEventListener("input", e => {
